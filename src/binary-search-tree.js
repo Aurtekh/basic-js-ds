7818cc44387f1b0524c5f8753edbac06aedf16fd
Daniil Ermolovich <ermolovichd007@gmail.com>
2022-10-17 23:49:47 +0300

    feat: add min and max

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,7 +6,7 @@ class Node {
     this.data = data;
     this.left = null;
     this.right = null;
-    this.parent = null;
+    // this.parent = null;
   }
 }
 
@@ -39,26 +39,53 @@ class BinarySearchTree {
           return;
         }
         thisNode = thisNode.left;
-        thisNode.parent = thisNode.data;
+        // thisNode.parent = thisNode.data;
       } else if (newNode.data > thisNode.data) {
         if (thisNode.right === null) {
           thisNode.right = newNode;
           return;
         }
         thisNode = thisNode.right;
-        thisNode.parent = thisNode.data;
+        // thisNode.parent = thisNode.data;
       }
     }
   }
-
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  preOrderDFS(node, callback) {
+    if (!node) {
+      return;
+    }
+    if (callback) {
+      callback(node);
+    }
+    this.preOrderDFS(node.left, callback);
+    this.preOrderDFS(node.right, callback);
+  }
+  has(data) {
+    return this.hasNode(this.rooot, data);
+  }
+  hasNode(thisNode, data) {
+    if (thisNode === null) return false;
+    if (thisNode.data === data) return true;
+    if (data < thisNode.data) {
+      return this.hasNode(thisNode.left, data)
+    }
+    if (data > thisNode.data) {
+      return this.hasNode(thisNode.right, data)
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return this.findNode(this.rooot, data);
+  }
+  findNode(thisNode, data) {
+    if (thisNode === null) return null;
+    if (thisNode.data === data) return thisNode;
+    if (data < thisNode.data) {
+      return this.findNode(thisNode.left, data)
+    }
+    if (data > thisNode.data) {
+      return this.findNode(thisNode.right, data)
+    }
   }
 
   remove(/* data */) {
@@ -67,14 +94,28 @@ class BinarySearchTree {
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.searchMinNode(this.rooot);
+  }
+  searchMinNode (thisNode) {
+    if (thisNode === null) return null;
+    if (thisNode.left === null) return thisNode.data;
+    if (thisNode.left.data < thisNode.data) {
+      return this.searchMinNode(thisNode.left)
+    }
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.searchMaxNode(this.rooot);
   }
+  searchMaxNode (thisNode) {
+    if (thisNode === null) return null;
+    if (thisNode.right === null) return thisNode.data;
+    if (thisNode.right.data > thisNode.data) {
+      return this.searchMaxNode(thisNode.right)
+    }
+  }
+
+
 }
 
 const visibleTree = new BinarySearchTree();
@@ -83,7 +124,8 @@ const visibleTree = new BinarySearchTree();
 // visibleTree.add(12);
 // visibleTree.add(14);
 // visibleTree.add(9);
-// visibleTree.root().value;
+// visibleTree.add(5);
+// visibleTree.add(3);
 // console.log(visibleTree);
 
 module.exports = {
