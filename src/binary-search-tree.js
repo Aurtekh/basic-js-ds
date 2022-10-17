18a5c91ceb8e895ce483d8a426097c809a555d2b
Daniil Ermolovich <ermolovichd007@gmail.com>
2022-10-17 22:13:17 +0300

    feat: add method add and root

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,4 +1,14 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
+
+
+class Node {
+  constructor (data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+    this.parent = null;
+  }
+}
 
 // const { Node } = require('../extensions/list-tree.js');
 
@@ -7,15 +17,38 @@ const { NotImplementedError } = require('../extensions/index.js');
 * using Node from extensions
 */
 class BinarySearchTree {
-
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor () {
+    this.rooot = null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  root() {
+    return this.rooot;
+  }
+
+  add(data) {
+    const newNode = new Node(data);
+    if(this.rooot === null) {
+      this.rooot = newNode;
+      return;
+    }
+    let thisNode = this.rooot;
+    while (thisNode) {
+      if (newNode.data < thisNode.data) {
+        if (thisNode.left === null) {
+          thisNode.left = newNode;
+          return;
+        }
+        thisNode = thisNode.left;
+        thisNode.parent = thisNode.data;
+      } else if (newNode.data > thisNode.data) {
+        if (thisNode.right === null) {
+          thisNode.right = newNode;
+          return;
+        }
+        thisNode = thisNode.right;
+        thisNode.parent = thisNode.data;
+      }
+    }
   }
 
   has(/* data */) {
@@ -43,6 +76,15 @@ class BinarySearchTree {
     // remove line with error and write your code here
   }
 }
+
+const visibleTree = new BinarySearchTree();
+
+// visibleTree.add(10);
+// visibleTree.add(12);
+// visibleTree.add(14);
+// visibleTree.add(9);
+// visibleTree.root().value;
+// console.log(visibleTree);
 
 module.exports = {
   BinarySearchTree
